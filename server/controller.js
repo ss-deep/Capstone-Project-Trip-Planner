1bd8e8aa7fb8b20eff2e296a4bab119a39506123
ss-deep <deepalisangole@gmail.com>
2024-04-16 02:34:35 -0500

    Did some formatting on the drog drop containers

--- server/controller.js
+++ server/controller.js
@@ -11,14 +11,17 @@ module.exports = {
             //call places api
             // let apiKey = "2edeea4d1a3e45f19e07523fdc852c2b";
             // console.log(req.body.properties.place_id);
-
-            let url = `https://api.geoapify.com/v2/places?categories=entertainment,national_park,natural&filter=place:${req.body.properties.place_id}&limit=2&format=json&apiKey=2edeea4d1a3e45f19e07523fdc852c2b`;
-            const attractionsList = await axios.get(`${url}`)
-            // console.log(attractionsList.data);
-            res.status(200).send(attractionsList.data);
+            if (req.body.properties.place_id!=='undefined') {
+                let url = `https://api.geoapify.com/v2/places?categories=entertainment,national_park,natural&filter=place:${req.body.properties.place_id}&limit=3&format=json&apiKey=2edeea4d1a3e45f19e07523fdc852c2b`;
+                const attractionsList = await axios.get(`${url}`)
+                // console.log(attractionsList.data);
+                res.status(200).send(attractionsList.data);
+            } else {
+                console.log("Enter the city name");
+            }
         } catch (error) {
-            console.error("ERROR GETTING DATA", error);
-            res.sendStatus(400);
+            // console.error("ERROR GETTING DATA", error);
+            // res.sendStatus(400);
         }
     }
 }
